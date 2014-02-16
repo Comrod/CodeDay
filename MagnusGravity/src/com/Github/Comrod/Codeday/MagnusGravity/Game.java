2cc16ca1b1355cf7deec2db4888a8088888cb22d
Comrod <icomrod@gmail.com>
2014-02-16 08:30:51 -0600

    Final commit

--- MagnusGravity/src/com/Github/Comrod/Codeday/MagnusGravity/Game.java
+++ MagnusGravity/src/com/Github/Comrod/Codeday/MagnusGravity/Game.java
@@ -2,22 +2,16 @@ package com.Github.Comrod.Codeday.MagnusGravity;
 
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
 
 import javax.swing.Timer;
 import javax.imageio.ImageIO;
-import javax.swing.Action;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements ActionListener
