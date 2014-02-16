b5ae7588a001b3156c9ac4425b4a4be3bdcd3b36
Comrod <icomrod@gmail.com>
2014-02-15 21:31:52 -0600

    Revert 397a549..6886d45
    This rolls back to commit 397a549a32a79bb60aff91ad184f0ac30e8df21a.

--- MagnusGravity/src/com/Github/Comrod/Codeday/MagnusGravity/Main.java
+++ MagnusGravity/src/com/Github/Comrod/Codeday/MagnusGravity/Main.java
@@ -4,10 +4,13 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 	
 	static JFrame frame = new JFrame();
+	
+	public static JLabel label = new JLabel();
 	
 	public static void main(String[] args)
 	{
@@ -16,6 +19,13 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		frame.add(label);
+		
+		
+		label.setVisible(true);
+		label.setText("Stuff");
+		label.setLocation(80, 80);
+		label.setSize(10, 30);
 		
 		RunGame();
 		
